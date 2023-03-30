273c736a0544f6c0ed1030bd8234ca14e694f4f0
Matthew Shaw <mshaw.cx@gmail.com>
2023-03-30 11:02:22 -0400

    add comments

--- jsxn.py
+++ jsxn.py
@@ -29,20 +29,24 @@ __all__ = ['jsxn']
 
 
 class _Jsxn:
+    # required for the derived class to be also be slotted
     __slots__ = []
 
     def __init__(self, *args, **kwds):
+        # call the instance with the passed arguments
         self(*args, **kwds)
+        # support partial initialization by setting undefined fields to null
         for attr in self.__slots__:
             if not hasattr(self, attr):
                 setattr(self, attr, None)
 
     def __call__(self, *args, **kwds):
+        # Calling jsxn instances accepts a variety of types. This block assigns
+        # values to the attributes based on the type of arguments passed in.
         if not args:
             args = kwds
         else:
             args = args[0]
-
         if isinstance(args, str):
             args = json.loads(args)
         if isinstance(args, dict):
@@ -51,12 +55,14 @@ class _Jsxn:
         elif not isinstance(args, list):
             raise TypeError('Invalid type') from None
 
+    # Support access attributes via indices.
     def __getitem__(self, name):
         return getattr(self, name)
 
     def __setitem__(self, name, value):
         setattr(self, name, value)
 
+    # This is for passing jsnx instances to the dict constructor.
     def __iter__(self):
         for attr in self.__slots__:
             yield((attr, getattr(self, attr)))
@@ -64,20 +70,27 @@ class _Jsxn:
     def __len__(self):
         return len(self.__slots__)
 
+    # Use JSON for string representations of the jsnx instance.
     def __str__(self):
         return json.dumps(dict(self))
 
 
+# The _Cache class is used to hold the generated classes. It is defined outside
+# of _JsxnFactory in order to reduce the potential for any name collisions with
+# the names used by consumers of the library.
 class _Cache(dict):
-    def create(self, name, *args, **kwds):
+    def generate(self, name, *args, **kwds):
+        # return the class if it has already been generated
         if name in self:
             return self[name]
 
+        # Creation of jsxn classes accepts a variety of types when specifying
+        # the schema of the class. This block derives the slots based on
+        # the arguments passed in.
         if not args:
             slots = kwds
         else:
             slots = args[0]
-
         if isinstance(slots, str):
             slots = json.loads(slots)
         if isinstance(slots, dict):
@@ -85,13 +98,23 @@ class _Cache(dict):
         elif not isinstance(slots, list):
             raise TypeError('Invalid type') from None
 
+        # Create the derived jsnx class.
         cls = type(name, (_Jsxn,), {'__slots__':slots})
+
+        # Cache the jsnx class.
         self[name] = cls
+
+        # Instantiate the class and return the instance.
         return cls(*args, **kwds)
 
+# Instantiate the cache dictionary.
 _cache = _Cache()
 
 
+# _JsxnFactory manages the creation and access to jsnx classes. When a derived
+# class is accessed it will return the cached jsnx class. If the jsnx class has
+# not been defined it will return a curried _Cache.generate function with the
+# attribute name bound as the first argument
 class _JsxnFactory:
     def __getitem__(self, name):
         return self.__getattr__(name)
@@ -100,7 +123,7 @@ class _JsxnFactory:
         try:
             return _cache[name]
         except KeyError:
-            return functools.partial(_cache.create, name)
+            return functools.partial(_cache.generate, name)
 
     def __delattr__(self, name):
         try:
@@ -112,4 +135,5 @@ class _JsxnFactory:
         del _cache[name]
 
 
+# This is the way to access the jsnx library.
 jsxn = _JsxnFactory()
