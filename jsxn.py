978ae1116cfeae22fee18bffbace505349d043a3
Matthew Shaw <mshaw.cx@gmail.com>
2025-06-09 21:22:15 -0400

    documentation and better argument handling

--- jsxn.py
+++ jsxn.py
@@ -30,7 +30,7 @@ __all__ = ['jsxn']
 
 
 class _Jsxn:
-    # required for the derived class to be also be slotted
+    # Required for the derived class to be also be slotted
     __slots__ = []
 
     def __init__(self, *args, **kwds):
@@ -46,11 +46,17 @@ class _Jsxn:
         # values to the attributes based on the type of arguments passed in.
         if not args:
             args = kwds
-        else:
+        elif len(args) == 1:
             args = args[0]
+        else:
+            raise ValueError('Only one unnamed argument can be passed')
 
+        # If args is string it must be JSON
         if isinstance(args, str):
-            args = json.loads(args)
+            try:
+                args = json.loads(args)
+            except json.decoder.JSONDecodeError:
+                raise ValueError('String arguments must be valid JSON')
 
         if isinstance(args, dict):
             for key,value in args.items():
@@ -58,30 +64,28 @@ class _Jsxn:
         elif isinstance(args, _Jsxn):
             for key,value in args:
                 setattr(self, key, value)
-        elif inspect.isclass(args):
-            pass
-        elif not isinstance(args, list):
-            raise TypeError('Invalid type') from None
+        else:
+            raise ValueError('Invalid type') from None
 
         return self
 
-    # Support access attributes via indices.
     def __getitem__(self, name):
+        # Support access attributes via indices.
         return getattr(self, name)
 
     def __setitem__(self, name, value):
         setattr(self, name, value)
 
-    # This is for passing jsxn instances to the dict constructor.
     def __iter__(self):
+        # This is for passing jsxn instances to the dict constructor.
         for attr in self.__slots__:
             yield((attr, getattr(self, attr)))
 
     def __len__(self):
         return len(self.__slots__)
 
-    # Use JSON for string representations of the jsxn instance.
     def __str__(self):
+        # Use JSON for string representations of the jsxn instance.
         return json.dumps(dict(self))
 
 
@@ -89,45 +93,55 @@ class _Jsxn:
 # of _JsxnFactory in order to reduce the potential for any name collisions with
 # the names used by consumers of the library.
 class _Cache(dict):
-    def _generate(self, _name_for_jsxn_class, *args, **kwds):
+    def _generate(self, _name, *args, **kwds):
         # Creation of jsxn classes accepts a variety of types when specifying
         # the schema of the class. This block derives the slots based on
         # the arguments passed in.
 
+        # Default to a bare jsxn class
         inherit = (_Jsxn,)
 
+        # Determine what is going to be used for slots
         if not args:
             slots = kwds
-        else:
+        elif len(args) == 1:
             if isinstance(args[0], type):
                 if not issubclass(args[0], _Jsxn):
                     inherit = (args[0],_Jsxn)
                 else:
                     inherit = (args[0],)
             slots = args[0]
+        else:
+            raise ValueError('Only one unnamed argument can be passed')
 
+        # If args is string it must be JSON
         if isinstance(slots, str):
-            slots = json.loads(slots)
+            try:
+                slots = json.loads(slots)
+            except json.decoder.JSONDecodeError:
+                raise ValueError('String arguments must be valid JSON')
 
+        # Use dictionary keys
         if isinstance(slots, dict):
             slots = tuple(slots.keys())
+        # Else use existing slots
         elif hasattr(slots, '__slots__'):
             slots = slots.__slots__
+        # Or derive from annotations
         elif hasattr(slots, '__annotations__'):
             slots = tuple(slots.__annotations__.keys())
+        # Finally assume it is a list
         elif not isinstance(slots, list):
-            raise TypeError('Invalid type') from None
+            raise ValueError('Invalid type')
 
-        # Create the derived jsxn class.
-        cls = type(_name_for_jsxn_class, tuple(inherit), {'__slots__':slots})
+        # Cache the derived jsxn class.
+        self[_name] = type(_name, tuple(inherit), {'__slots__':slots})
 
-        # Cache the jsxn class.
-        self[_name_for_jsxn_class] = cls
-        return cls
+        return self[_name]
 
-    def _instantiate(self, _name_for_jsxn_class, *args, **kwds):
-        cls = self._generate(_name_for_jsxn_class, *args, **kwds)
-        # Instantiate the class and return the instance.
+    def _instantiate(self, _name, *args, **kwds):
+        # Return the instantiated class
+        cls = self._generate(_name, *args, **kwds)
         return cls(*args, **kwds)
 
 
@@ -159,31 +173,35 @@ class _JsxnFactory:
             def wrap(cls):
                 return inject(cls.__name__, cls)
             return wrap
+        # Use the class name when not called as a function
         elif inspect.isclass(arg):
             return inject(arg.__name__, arg)
-
+        # Raise error in all other circumstances
         raise TypeError(arg)
 
     def __getattr__(self, name):
+        # Access jsxn classes via attribute
         try:
             self.__getattribute__(name)
         except AttributeError:
             return self[name]
 
     def __getitem__(self, name):
+        # Return existing or instantiate a new jsxn class
         try:
-            cls = _cache[name]
-            return cls
+            return _cache[name]
         except KeyError:
             return functools.partial(_cache._instantiate, name)
 
     def __delattr__(self, name):
+        # Support deleting jsxn classes
         try:
             del _cache[name]
         except KeyError:
             raise AttributeError(name) from None
 
     def __delitem__(self, name):
+        # Support deleting jsxn classes
         del _cache[name]
 
 # This is the way to access the jsxn library.
