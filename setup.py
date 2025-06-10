f82131f25958bb53200dfe0e1a368818f69d04f3
Matthew Shaw <mshaw.cx@gmail.com>
2025-06-09 20:38:31 -0400

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name             = 'jsxn',
-    version          = '0.2',
+    version          = '0.3',
     author           = 'Matthew Shaw',
     author_email     = 'mshaw.cx@gmail.com',
     license          = 'MIT',
